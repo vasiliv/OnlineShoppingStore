8dfe8e178dcbc150159339585fb7f37458e1d891
vasiliv <ggo24@yahoo.com>
2019-07-22 21:13:08 +0400

    5.2

--- OnlineShoppingstore.WebUI/Controllers/ProductController.cs
+++ OnlineShoppingstore.WebUI/Controllers/ProductController.cs
@@ -6,22 +6,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace OnlineShoppingstore.WebUI.Controllers
-{
-    public class ProductController : Controller
-    {
+namespace OnlineShoppingstore.WebUI.Controllers {
+    public class ProductController : Controller {
         private readonly IProductRepository repository;
         public int PageSize = 4;
         public ProductController(IProductRepository repo) {
             repository = repo;
         }
-        public ViewResult List(int page = 1)
-        {
-            ProductsListViewModel model = new ProductsListViewModel();
-            return View(repository.Products
+        public ViewResult List(int page = 1) {
+            ProductsListViewModel model = new ProductsListViewModel {
+                Products = repository.Products
                         .OrderBy(p => p.Id)
-                        .Skip((page -1) * PageSize)
-                        .Take(PageSize));
+                        .Skip((page - 1) * PageSize)
+                        .Take(PageSize),
+                PagingInfo = new PagingInfo {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
--- OnlineShoppingstore.WebUI/HtmlHelpers/PagingHelper.cs
+++ OnlineShoppingstore.WebUI/HtmlHelpers/PagingHelper.cs
@@ -12,7 +12,7 @@ namespace OnlineShoppingstore.WebUI.HtmlHelpers {
                                               PagingInfo pagingInfo,
                                               Func<int,string> pageUrl) {
             StringBuilder result = new StringBuilder();
-            for (int i = 1; i < pagingInfo.TotalPages; i++) {
+            for (int i = 1; i <= pagingInfo.TotalPages; i++) {
                 TagBuilder tag = new TagBuilder("a");
                 tag.MergeAttribute("href", pageUrl(i));
                 tag.InnerHtml = i.ToString();
